1109d11d155ef139462c9d9700656a39b8e7673b
Chirayu Desai <chirayudesai1@gmail.com>
2021-02-10 00:21:11 +0530

    Import NetworkPolicyManager for testing SystemApi
    Change-Id: I6d5ff6898faf37df6baf0421feac1e45ee079f7d

--- app/src/main/java/org/calyxos/firewall/MainActivity.java
+++ app/src/main/java/org/calyxos/firewall/MainActivity.java
@@ -3,6 +3,7 @@ package org.calyxos.firewall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.net.NetworkPolicyManager;
 
 public class MainActivity extends AppCompatActivity {
 
