694d421c284c2eda68778907268db59d0f82b81b
Ese Udom <udomese@yahoo.com>
2021-02-11 19:56:55 +0100

    Create UIs and UI resources
    Change-Id: I815badb8951f67c684c5e9a1a9d82ad7c5816a73

--- app/src/main/java/org/calyxos/firewall/MainActivity.java
+++ app/src/main/java/org/calyxos/firewall/MainActivity.java
@@ -1,15 +1,29 @@
 package org.calyxos.firewall;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.net.NetworkPolicyManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+    //private SystemAppsAdapter mAdapter;
+    //private InstalledAppsAdapter mAdapter1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView = findViewById(R.id.system_apps);
     }
 }
