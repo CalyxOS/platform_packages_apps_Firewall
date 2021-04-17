391987f17880f3f1fde96cbfc1ac8901ae2c11d7
Ese Udom <udomese@yahoo.com>
2021-04-17 09:58:26 +0100

    Implement Global Cleartext traffic settings
    Change-Id: Ibb0e946c535b658159b27226320417a32613c541

--- app/src/main/java/org/calyxos/datura/settings/SettingsManager.java
+++ app/src/main/java/org/calyxos/datura/settings/SettingsManager.java
@@ -1,7 +1,12 @@
 package org.calyxos.datura.settings;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.LinkProperties;
+import android.net.Network;
 import android.net.NetworkPolicyManager;
+import android.os.StrictMode;
+import android.provider.Settings;
 import android.util.SparseIntArray;
 
 import static android.net.NetworkPolicyManager.POLICY_ALLOW_METERED_BACKGROUND;
@@ -68,6 +73,29 @@ public class SettingsManager {
         return getAppRestriction(uid, POLICY_REJECT_WIFI);
     }
 
+    public boolean isPrivateDNSEnabled() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null) {
+            LinkProperties linkProperties = connectivityManager.getLinkProperties(network);
+            if (linkProperties != null) {
+                return linkProperties.isPrivateDnsActive();
+            }
+        }
+        return false;
+    }
+
+    public void blockCleartextTraffic(boolean block) {
+        Settings.Global.putInt(mContext.getContentResolver(), Settings.Global.CLEARTEXT_NETWORK_POLICY,
+                block? StrictMode.NETWORK_POLICY_REJECT : StrictMode.NETWORK_POLICY_ACCEPT);
+    }
+
+    //might come in handy
+    private boolean isCleartextBlocked() {
+        return Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.CLEARTEXT_NETWORK_POLICY, StrictMode.NETWORK_POLICY_ACCEPT)
+                != StrictMode.NETWORK_POLICY_ACCEPT;
+    }
+
     private boolean getAppRestriction(int uid, int policy) {
         final int uidPolicy = mPolicyManager.getUidPolicy(uid);
         return (uidPolicy & policy) != 0;
@@ -96,4 +124,38 @@ public class SettingsManager {
             mPolicyManager.removeUidPolicy(uid, policy);
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    public boolean isPrivateDNSEnabled() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null) {
+            LinkProperties linkProperties = connectivityManager.getLinkProperties(network);
+            if (linkProperties != null) {
+                return linkProperties.isPrivateDnsActive();
+            }
+        }
+        return false;
+    }
+
+    public void blockCleartextTraffic(boolean block) {
+        SystemProperties.set(StrictMode.CLEARTEXT_PROPERTY, Boolean.toString(block));
+        Settings.Global.putInt(mContext.getContentResolver(), Settings.Global.CLEARTEXT_NETWORK_POLICY,
+                block? StrictMode.NETWORK_POLICY_REJECT : StrictMode.NETWORK_POLICY_ACCEPT);
+    }
+
+    public boolean isCleartextBlocked() {
+        return Settings.Global.getInt(mContext.getContentResolver(), Settings.Global.CLEARTEXT_NETWORK_POLICY, StrictMode.NETWORK_POLICY_ACCEPT)
+                != StrictMode.NETWORK_POLICY_ACCEPT;
+    }
+
+    public void allowAppCleartext(int uid, boolean allow) throws RemoteException {
+        setAppCleartextPolicy(uid, allow? StrictMode.NETWORK_POLICY_ACCEPT : StrictMode.NETWORK_POLICY_REJECT);
+    }
+
+    private void setAppCleartextPolicy(int uid, int policy) throws RemoteException {
+        netd.setUidCleartextNetworkPolicy(uid, policy);
+    }
+>>>>>>> Stashed changes
 }
