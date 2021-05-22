a685508cc7334f4ed8a31e9ad2a06439e498abe3
Ese Udom <udomese@yahoo.com>
2021-05-22 22:35:24 +0100

    Add the global toggles in a list
    Change-Id: I518de3caa8d71b62374d263eeef6982ad0171b71

--- app/src/main/java/org/calyxos/datura/MainActivity.java
+++ app/src/main/java/org/calyxos/datura/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -18,9 +19,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.calyxos.datura.adapter.AppAdapter;
+import org.calyxos.datura.adapter.GlobalSettingsAdapter;
 import org.calyxos.datura.fragment.AboutDialogFragment;
 import org.calyxos.datura.settings.SettingsManager;
 import org.calyxos.datura.util.Util;
@@ -34,12 +37,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private RecyclerView mAppList;
     private AppAdapter mAppAdapter;
+    private GlobalSettingsAdapter mGlobalSettingsAdapter;
     private EditText mSearchBar;
     private ImageView mSearchIcon, mSearchClear;
 
     private SwitchCompat mCleartextToggle;
-
     private SettingsManager mSettingsManager;
+    private static MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +88,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAppList = findViewById(R.id.app_list);
 
         mSettingsManager = new SettingsManager(this);
+
+        mainActivity = this;
     }
 
     @Override
@@ -91,11 +97,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onResume();
 
         final PackageManager pm = getPackageManager();
-
-        //check if Private DNS is enabled
-        mCleartextToggle.setEnabled(mSettingsManager.isPrivateDNSEnabled());
-        //initialize cleartext toggle state
-        mCleartextToggle.setChecked(mSettingsManager.isCleartextBlocked());
 
         //To avoid search result list and real list mix up
         if (mSearchBar.getVisibility() == View.VISIBLE && !mSearchBar.getText().toString().isEmpty())
@@ -121,7 +122,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if (mAppAdapter == null) {
             mAppAdapter = new AppAdapter(this, pm, instApps, sysApps);
-            mAppList.setAdapter(mAppAdapter);
+            mGlobalSettingsAdapter = new GlobalSettingsAdapter(this, pm);
+            ConcatAdapter concatAdapter = new ConcatAdapter(mGlobalSettingsAdapter, mAppAdapter);
+            mAppList.setAdapter(concatAdapter);
         } else
             mAppAdapter.notifyDataSetChanged();
     }
@@ -205,13 +208,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mSearchBar.setText("");
                 break;
             }
-
-            case R.id.global_cleartext_toggle: {
-                mSettingsManager.blockCleartextTraffic(mCleartextToggle.isChecked());
-                if (mAppAdapter != null)
-                    mAppAdapter.notifyDataSetChanged(); //NOTE include this in a thread/service as well
-                break;
-            }
         }
+    }
+
+    public static MainActivity getInstance() {
+        return mainActivity;
+    }
+
+    public void notifyDataSetChanged() {
+        if (mAppAdapter != null)
+            mAppAdapter.notifyDataSetChanged(); //NOTE include this in a thread/service as well
     }
 }
---
+++ app/src/main/java/org/calyxos/datura/adapter/GlobalSettingsAdapter.java
@@ -0,0 +1,87 @@
+package org.calyxos.datura.adapter;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.calyxos.datura.MainActivity;
+import org.calyxos.datura.R;
+import org.calyxos.datura.settings.SettingsManager;
+
+public class GlobalSettingsAdapter extends RecyclerView.Adapter<GlobalSettingsAdapter.ViewHolder> {
+
+    private static final String TAG = GlobalSettingsAdapter.class.getSimpleName();
+    private final Context mContext;
+    private final PackageManager mPackageManager;
+    private final SettingsManager mSettingsManager;
+
+    public GlobalSettingsAdapter(Context context, PackageManager packageManager) {
+        mContext = context;
+        mPackageManager = packageManager;
+        mSettingsManager = new SettingsManager(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_global_settings, parent, false);
+        return new ViewHolder(view, mContext, mPackageManager, mSettingsManager);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private Context mContext;
+        private PackageManager mPackageManager;
+        private SettingsManager mSettingsManager;
+        private SwitchCompat mClrTextToggle;
+
+        public ViewHolder(@NonNull View itemView, Context context, PackageManager packageManager, SettingsManager settingsManager) {
+            super(itemView);
+
+            mContext = context;
+            mPackageManager = packageManager;
+            mSettingsManager = settingsManager;
+
+            mClrTextToggle = itemView.findViewById(R.id.global_cleartext_toggle);
+
+            //check if Private DNS is enabled
+            mClrTextToggle.setEnabled(mSettingsManager.isPrivateDNSEnabled());
+            //initialize cleartext toggle state
+            mClrTextToggle.setChecked(mSettingsManager.isCleartextBlocked());
+
+            //set on click listeners instead of checked change for actual settings API calls because known issues
+            //that comes with that
+            mClrTextToggle.setOnClickListener(this);
+        }
+
+        public void bind(ApplicationInfo app) {
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v.equals(mClrTextToggle)) {
+                mSettingsManager.blockCleartextTraffic(mClrTextToggle.isChecked());
+                //call a main activity function that refreshes the list of apps
+                MainActivity.getInstance().notifyDataSetChanged();
+            }
+        }
+    }
+}
