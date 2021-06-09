d8af2203a5919332e65555b805e1e5922d04594c
Ese Udom <udomese@yahoo.com>
2021-06-09 16:13:19 +0100

    Add a notification for the default config for new apps
    * Notification to tie to the foreground service in previous commit
    
    Change-Id: Iba711b2180f5dd55541269486f31ff93a37a3b14

--- app/src/main/java/org/calyxos/datura/service/DefaultConfigService.java
+++ app/src/main/java/org/calyxos/datura/service/DefaultConfigService.java
@@ -1,15 +1,27 @@
 package org.calyxos.datura.service;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import org.calyxos.datura.MainActivity;
 import org.calyxos.datura.NewPackageInstallReceiver;
+import org.calyxos.datura.R;
+import org.calyxos.datura.util.Constants;
+
+import static android.app.Notification.EXTRA_NOTIFICATION_ID;
 
 public class DefaultConfigService extends Service {
 
@@ -23,8 +35,49 @@ public class DefaultConfigService extends Service {
 
     private final ServiceBinder binder = new ServiceBinder();
 
-    private Notification getNotification() {
-        return null; //TODO build a notification
+    public Notification getNotification() {
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(R.string.default_config_notification_title))
+                .setContentText(getString(R.string.default_config_notification_desc))
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.default_config_notification_desc)))
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_REMINDER)
+                .setContentIntent(getPendingIntent())
+                .addAction(R.drawable.ic_close_24, getString(R.string.stop), getStopActionPendingIntent());
+
+        Notification notification = builder.build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(Constants.DEFAULT_CONFIG_NOTIFICATION_ID, notification);
+
+        return notification;
+    }
+
+    private void createNotificationChannel() {
+        CharSequence name = getString(R.string.default_config_notification_channel_name);
+        String description = getString(R.string.default_config_notification_channel_description);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return PendingIntent.getActivity(this, 0, intent, 0);
+    }
+
+    private PendingIntent getStopActionPendingIntent() {
+        Intent stopIntent = new Intent(this, DefaultConfigService.DismissActionReceiver.class);
+        stopIntent.setAction(Constants.ACTION_STOP);
+        stopIntent.putExtra(EXTRA_NOTIFICATION_ID, Constants.DEFAULT_CONFIG_NOTIFICATION_ID);
+        return PendingIntent.getBroadcast(this, 0, stopIntent, 0);
     }
 
     public void stopService() {
@@ -53,5 +106,19 @@ public class DefaultConfigService extends Service {
         if (newPackageInstallReceiver != null)
             unregisterReceiver(newPackageInstallReceiver);
         stopForeground(true);
+    }
+
+    class DismissActionReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Constants.ACTION_STOP)) {
+                int notificationId = intent.getExtras().getInt(EXTRA_NOTIFICATION_ID);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(DefaultConfigService.this);
+                notificationManager.cancel(notificationId);
+                stopService();
+            }
+        }
     }
 }
--- app/src/main/java/org/calyxos/datura/util/Constants.java
+++ app/src/main/java/org/calyxos/datura/util/Constants.java
@@ -10,4 +10,8 @@ public class Constants {
     public static final String ALLOW_VPN_DATA = "allow_vpn_data";
     public static final String ALLOW_CLEARTEXT_TRAFFIC = "allow_cleartext_traffic";
     public static final int DEFAULT_CONFIG_NOTIFICATION_ID = 5;
+    public static final String NOTIFICATION_CHANNEL_ID = "org.calyxos.datura.default_config_channel";
+    public static final String NOTIFICATION_CHANNEL_GROUP_ID = "org.calyxos.datura.channel_group";
+    public static final String ACTION_STOP = "action_dismiss";
+
 }
