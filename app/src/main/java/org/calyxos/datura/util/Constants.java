e1baddbe127881aeb0cbec00cd84c025939dbf7e
Ese Udom <udomese@yahoo.com>
2021-06-24 18:17:08 +0100

    Persist sort menu item actions
    Issue: calyxos#455
    Change-Id: I23c14319faaa7d5ba6863ab749a639e55fccf9bb

---
+++ app/src/main/java/org/calyxos/datura/util/Constants.java
@@ -0,0 +1,8 @@
+package org.calyxos.datura.util;
+
+public class Constants {
+    public static final String SORT_PREFERENCE = "org.calyxos.datura.sort_preference";
+    public static final String SORT_BY = "sort_by";
+    public static final String NAME = "name";
+    public static final String LAST_USED = "last_used";
+}
